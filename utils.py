0f0b7f56dc37c59a0439eb75f9596a46dbb55e5c
沈庾涛 <sanyito@shenyutaodeMacBook-Pro.local>
2020-02-28 10:26:55 +0800

    modify logger para

--- utils.py
+++ utils.py
@@ -27,7 +27,7 @@ def convert_state_dict(state_dict):
 
 
 def get_logger(logdir):
-    logger = logging.getLogger("lgnet")
+    logger = logging.getLogger("CNN")
     ts = str(datetime.datetime.now()).split(".")[0].replace(" ", "_")
     ts = ts.replace(":", "_").replace("-", "_")
     file_path = os.path.join(logdir, "run_{}.log".format(ts))
